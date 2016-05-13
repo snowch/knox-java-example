c2b0e65adbf7d1cfe68932f68aaf099e26a53b7b
christopher snow <chsnow123@gmail.com>
2016-05-13 07:35:39 +0100

    initial import

---
+++ src/main/java/net/christophersnow/TestKnox.java
@@ -0,0 +1,17 @@
+package net.christophersnow;
+
+import org.apache.hadoop.gateway.shell.Hadoop;
+import org.apache.hadoop.gateway.shell.hdfs.Hdfs;
+
+public class TestKnox {
+
+    public static void main(String[] args) {
+
+    	Hadoop session = Hadoop.login( "test", "test", "test" );
+    	
+    	String text = Hdfs.get( session ).from( "test" ).now().getString();
+    	
+    	session.shutdown();
+    }
+
+}
